1c5b4b89f2d0b9056e29c5e8f3b5452676da6118
melaniezhou1 <72285795+melaniezhou1@users.noreply.github.com>
2022-02-25 14:11:50 -0800

    initial environment setup

---
+++ Themes/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+    green: "#3D9A5F",
+    blue: "#2D60B3",
+    black: "#2D60B3",
+    magenta: "#C70A72",
+    white: "#FFFFFF",
+    black: "#000000",
+  };
+  
+  export default colors;
+  
