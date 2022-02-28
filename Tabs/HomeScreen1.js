4e7b7e2e52864a39dc1f0d90b89542b2737ae9c1
melaniezhou1 <melaniezhou1@gmail.com>
2022-02-28 14:49:59 -0800

    Moved working home screen UI to HomeScreen1

---
+++ Tabs/HomeScreen1.js
@@ -0,0 +1,84 @@
+import { StatusBar } from 'expo-status-bar';
+import { createPortal } from 'react-dom';
+import { StyleSheet, Text, View, Image, FlatList, SafeAreaView, Button, TouchableWithoutFeedback } from "react-native";
+import colors from './Themes/colors.js';
+import { useFonts } from 'expo-font';
+import {ImageBackground} from 'react-native' ;
+import home from  './assets/home.png'
+import favicon from './assets/favicon.png'
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={home} style={styles.bgimg}>
+        <Text style = {styles.blacktext}>
+          Favorites | Explore 
+        </Text>
+
+        <Text style={styles.whitetext}>
+          @worldwildlifefund
+        </Text>
+        <Text style={styles.whitetextsmall}>
+          Today Suyash explores the Sahara. Save the Animals Fundraiser.
+        </Text>
+        <Image source = {require('./assets/wwflogo.png')} style = {styles.sideiconprof}> 
+        </Image>
+        <Image source = {require('./assets/heart.png')} style = {styles.sideicon}> 
+        </Image>
+        <Image source = {require('./assets/xicon.png')} style = {styles.sideicon}> 
+        </Image>
+        <Image source = {require('./assets/share.png')} style = {styles.sideicon}> 
+        </Image>
+        </ImageBackground>
+        <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  bgimg: {
+    display: "flex",
+    width: '100%',
+    height: '100%',
+    position: 'relative',
+  }, 
+  sideicon: {
+    borderRadius: 100,
+    margin: 10,
+    top: '55%',
+    left: '83%',
+  }, 
+  sideiconprof: {
+    borderRadius: 100,
+    margin: 7,
+    top: '55%',
+    left: '81%',
+  }, 
+  whitetext: {
+    top: '87%',
+    left: '5%',
+    fontSize: 20,
+    color: 'white',
+  },
+  whitetextsmall: {
+    margin: 5,
+    top: '87%',
+    left: '5%',
+    fontSize: 16,
+    color: 'white',
+  },
+  blacktext: {
+    letterSpacing: 4,
+    top: '7%',
+    left: '22%',
+    color: 'black',
+    fontSize: 20,
+  },
+});
