0ad346c531302edb4247e6e705541e5bc5798780
melaniezhou1 <melaniezhou1@gmail.com>
2022-02-28 14:24:22 -0800

    finished feed page #1

--- App.js
+++ App.js
@@ -12,14 +12,26 @@ export default function App() {
   return (
     <View style={styles.container}>
       <ImageBackground source={home} style={styles.bgimg}>
-      <Text style={styles.whitetext}>
-        @worldwildlifefund
-      </Text>
-      <Text style={styles.whitetextsmall}>
-        Today Suyash explores the Sahara. Save the Animals Fundraiser.
-      </Text>
-      </ImageBackground>
-      <StatusBar style="auto" />
+        <Text style = {styles.blacktext}>
+          Favorites | Explore 
+        </Text>
+
+        <Text style={styles.whitetext}>
+          @worldwildlifefund
+        </Text>
+        <Text style={styles.whitetextsmall}>
+          Today Suyash explores the Sahara. Save the Animals Fundraiser.
+        </Text>
+        <Image source = {require('./assets/wwflogo.png')} style = {styles.sideiconprof}> 
+        </Image>
+        <Image source = {require('./assets/heart.png')} style = {styles.sideicon}> 
+        </Image>
+        <Image source = {require('./assets/xicon.png')} style = {styles.sideicon}> 
+        </Image>
+        <Image source = {require('./assets/share.png')} style = {styles.sideicon}> 
+        </Image>
+        </ImageBackground>
+        <StatusBar style="auto" />
     </View>
   );
 }
@@ -37,17 +49,36 @@ const styles = StyleSheet.create({
     height: '100%',
     position: 'relative',
   }, 
+  sideicon: {
+    borderRadius: 100,
+    margin: 10,
+    top: '55%',
+    left: '83%',
+  }, 
+  sideiconprof: {
+    borderRadius: 100,
+    margin: 7,
+    top: '55%',
+    left: '81%',
+  }, 
   whitetext: {
     top: '87%',
     left: '5%',
     fontSize: 20,
     color: 'white',
   },
-
   whitetextsmall: {
+    margin: 5,
     top: '87%',
     left: '5%',
     fontSize: 16,
     color: 'white',
+  },
+  blacktext: {
+    letterSpacing: 4,
+    top: '7%',
+    left: '22%',
+    color: 'black',
+    fontSize: 20,
   },
 });
