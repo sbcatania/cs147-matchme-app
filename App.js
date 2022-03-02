4168102a71175e8ccdca0b01a310c652c7e0a8b6
melaniezhou1 <melaniezhou1@gmail.com>
2022-03-02 14:06:00 -0800

    Added import statements for navigation and updated readme with instructions

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { createPortal } from 'react-dom';
 import { StyleSheet, Text, View, Image, FlatList, SafeAreaView, Button, TouchableWithoutFeedback } from "react-native";
 import { useFonts } from 'expo-font';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './Tabs/Home';
 
 
--- README.md
+++ README.md
@@ -12,6 +12,8 @@ Run `expo start`
 ## To install navigator and make sure import statements work
 Run 'expo install react-native-screens react-native-safe-area-context'
 Then run 'expo install react-native-gesture-handler'
+Then run 'npm install'
+Then run 'expo start'
 
 ## To install pager-view from home screen
 Run 'expo install react-native-pager-view'
