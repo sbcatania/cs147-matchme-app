582ae8ffc46da12f3699c978ae4e2232c1e18eec
melaniezhou1 <melaniezhou1@gmail.com>
2022-03-04 01:06:51 -0800

    building fundraiser page

--- Tabs/Donation/fundraiser.js
+++ Tabs/Donation/fundraiser.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { createPortal } from 'react-dom';
-import { StyleSheet, Text, ImageBackground, View, Image, TouchableOpacity, FlatList, SafeAreaView, Button, TouchableWithoutFeedback } from "react-native";
+import { StyleSheet, Text, ImageBackground, Pressable, View, Image, TouchableOpacity, ScrollView, SafeAreaView, Button, TouchableWithoutFeedback } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -14,13 +14,26 @@ import Explore from './Explore'
 export default function Fundraiser() {
     const navigation = useNavigation();
     return(
-      <View style = {styles.container}>
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.scrollView}>
+
         <ImageBackground source={moose} style = {styles.bgimg} >
-        <TouchableOpacity style = {styles.button} onPress={()=>navigation.navigate(Explore)}>
+        <TouchableOpacity style = {styles.backbutton} onPress={()=>navigation.navigate(Explore)}>
             <Image source={require("../../assets/Donation/arrow.png")}/>
-          </TouchableOpacity>
+        </TouchableOpacity>
+        <Image source = {require('../../assets/Home/wwflogo.png')} style = {styles.sideiconprof}> 
+        </Image>
+        <Text style = {styles.whitetext}> Save the Animals </Text>
+        <Text style = {styles.blacktext}> World Wildlife Fund </Text>
+        <Text style = {styles.blacktext}> February 2022 - March 2023  </Text>
+        <Pressable style={styles.donatebutton} onPress={() => navigation.navigate(DonationEntry)}>
+            <Text style={styles.buttontext}> DONATE </Text>
+            
+    </Pressable>
+
         </ImageBackground>
-      </View>
+        </ScrollView>
+      </SafeAreaView>
 
     );
 }
@@ -30,21 +43,54 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: COLORS.WHITE,
     },
-    button: {
+    scrollView: {
+      backgroundColor: 'white',
+    },
+    backbutton: {
       top: '10%',
+    },
+    donatebutton: {
+      top: '20%',
+      left: '22%',
+      width: 250,
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 12,
+      borderRadius: 100,
+      elevation: 3,
+      backgroundColor: COLORS.GREEN,
+    },
+    buttontext: {
+      color: COLORS.WHITE,
+      fontWeight: 'bold',
+      fontSize: 35,
     },
   
     bgimg: {
       display: "flex",
       width: '100%',
-      height: '60%',
+      height: '80%',
       position: 'relative',
     },
+    sideiconprof: {
+        borderRadius: 100,
+        margin: 7,
+        top: '40%',
+        left: '8%',
+        height: 100,
+        width: 100,
+    }, 
+    whitetext: {
+      top: '2%',
+      left: '36%',
+      fontSize: 30,
+      color: 'white',
+      fontWeight: 'bold',
+    },
     blacktext: {
-        letterSpacing: 4,
-        top: '7%',
-        left: '20%',
-        color: 'black',
-        fontSize: 20,
-      },
+      top: '4%',
+      left: '36%',
+      fontSize: 18,
+      color: 'black',
+    },
 });
