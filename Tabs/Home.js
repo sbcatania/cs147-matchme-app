a6c838af7cbabbb3ed20d3fb8e3821aeee0fbb98
sbcatania <49293867+sbcatania@users.noreply.github.com>
2022-03-05 15:32:59 -0800

    Recreated explore.js to use json-stored values and set up constants correctly

--- Tabs/Home.js
+++ Tabs/Home.js
@@ -1,17 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { createPortal } from 'react-dom';
 import { StyleSheet, Text, View, Image, TouchableOpacity, FlatList, SafeAreaView, Button, TouchableWithoutFeedback } from "react-native";
-import { useFonts } from 'expo-font';
-import {ImageBackground} from 'react-native' ;
-import PagerView from "react-native-pager-view";
-import { useState, useEffect } from "react";
-import home from  '../assets/Home/home.png';
-import home2 from  '../assets/Home/home2.png';
-import home3 from  '../assets/Home/home3.jpg';
-import home4 from  '../assets/Home/home4.jpg';
-import home5 from  '../assets/Home/home5.jpg';
-import { NavigationContainer } from '@react-navigation/native';
-import { useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Explore from './Donation/Explore';
 import Fundraiser from './Donation/Fundraiser';
@@ -23,49 +10,7 @@ import DonationThankYou from './Donation/DonationThankYou';
 const LOAD_TIME = 100;
 const INTERVAL = 25;
 
-// Constants for page layout
-// const TOP_TEXT = "Explore";
-// const LIKE_ICON = '../assets/Home/heart.png';
-// const DISLIKE_ICON = '../assets/Home/xicon.png';
-// const SHARE_ICON = '../assets/Home/share.png';
-// const DONATE_ICON = '../assets/Navigation/donate.png';
-
-// function createPost(post_number) {
-//   const navigation = useNavigation();
-//   let post = JSON.parse(DATA.POSTS[post_number]);
-//   console.log(post);
-//   return(
-//     <View style={styles.container}>
-//         <ImageBackground source={post.post_image} style={styles.bgimg}>
-//           <Text style = {styles.blacktext}>
-//             {TOP_TEXT}
-//           </Text>
-
-//           <Text style={styles.whitetext}>
-//             {post.handle}
-//           </Text>
-//           <Text style={styles.whitetextsmall}>
-//             {post.caption}
-//           </Text>
-//           <Image source = {require(post.profile_image)} style = {styles.sideiconprof}> 
-//           </Image>
-//           <Image source = {require(LIKE_ICON)} style = {styles.sideicon}> 
-//           </Image>
-//           <Image source = {require(DISLIKE_ICON)} style = {styles.sideicon}> 
-//           </Image>
-//           <Image source = {require(SHARE_ICON)} style = {styles.sideicon}> 
-//           </Image>
-//           <TouchableOpacity style = {styles.button} onPress={()=>navigation.navigate(post.donation_link)}>
-//             <Image style = {styles.donateimg} source={require(DONATE_ICON)}/>
-//           </TouchableOpacity>
-//           </ImageBackground>
-//           <StatusBar style="auto" />
-//       </View>
-//     );
-// }
-
 const Stack = createNativeStackNavigator();
-
 // turn this into a stack navigator 
 export default function Home() {
   return (
--- Themes/constants.js
+++ Themes/constants.js
@@ -8,7 +8,33 @@ export const COLORS = {
   };
 
 export const DATA = {
-    DONATION_HISTORY: './assets/Data/donation-history.json',
-    POSTS: './assets/Data/posts.json',
-    PROFILES: './assets/Data/profile.json',
+    POSTS     : require('../assets/Data/posts.json'),
+    // DONATION_HISTORY: require('../assets/Data/donation-history.json'),
+    // PROFILES  : './assets/Data/profile.json',
   };
+
+export const IMAGES = {
+    // Home images
+    LIKE_ICON     : require('../assets/Home/heart.png'),
+    DISLIKE_ICON  : require('../assets/Home/xicon.png'),
+    SHARE_ICON    : require('../assets/Home/share.png'),
+    DONATE_ICON   : require('../assets/Navigation/donate.png'),
+
+    // Feed images, key is the post number
+    HOME: {
+        1: require('../assets/Home/home.png'),
+        2: require('../assets/Home/home2.png'),
+        3: require('../assets/Home/home3.jpg'),
+        4: require('../assets/Home/home4.jpg'),
+        5: require('../assets/Home/home5.jpg'),
+    },
+
+    // Profile images, key is handle
+    PROFILE: {
+        'worldwildlifefund' : require('../assets/Home/wwflogo.png'),
+        'malalafund'        : require('../assets/Home/malala.jpg'),
+        'savethebees'       : require('../assets/Home/bees.png'),
+        'paloaltoshelter'   : require('../assets/Home/shelter.jpg'),
+    },
+    
+}
