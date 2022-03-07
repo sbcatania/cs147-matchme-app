9e335355246a818102e5249d3026458c504b0c32
Lily Taylor <lilianajtaylor@stanford.edu>
2022-03-07 15:57:30 -0800

    Nvm disregard

--- Tabs/Profile.js
+++ Tabs/Profile.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { createPortal } from 'react-dom';
-import { StyleSheet, Text, View, Image, TouchableOpacity, FlatList, SafeAreaView, Button, TouchableWithoutFeedback } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity, FlatList, SafeAreaView, Button, TouchableWithoutFeedback, Pressable } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useState } from 'react';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
+
+
+
+// CURRENTLY USING TO DEVELOP NFP RWDS PAGE
 
 export default function Fundraiser() {
     return(
         <Text style = {styles.blacktext}> Profile </Text>
-    );
+        );
 }
 
 const styles = StyleSheet.create({
