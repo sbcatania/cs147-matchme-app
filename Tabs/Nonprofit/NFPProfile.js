8f40966c01c3396c37e67858514274f7c9c0488d
Lily Taylor <lilianajtaylor@stanford.edu>
2022-03-09 16:55:40 -0800

    C/P template from donor flow

--- Tabs/Nonprofit/NFPProfile.js
+++ Tabs/Nonprofit/NFPProfile.js
@@ -1,25 +1,28 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from 'react';
+import { StyleSheet, Text, ImageBackground, Pressable, View, Image, TouchableOpacity, ScrollView, SafeAreaView, Button, TouchableWithoutFeedback } from "react-native";
 import { useNavigation } from '@react-navigation/native';
-import { useState } from 'react';
-import { COLORS } from '../../Constants';
+import { useRoute } from "@react-navigation/native";
+import { COLORS, DATA } from '../../Constants';
 
 /* 
 * TODO: 
 *   - Implement NFP profile page.
 *   - WHITE BACKGROUND - do the hacky safe area view thing to get a completely white bacgkround
 */
-
-export default function Fundraiser() {
-  
+//export default function NFPFundraiser({route}) {
+export default function NFPFundraiser() {
 
   // NAVIGATION: Setup nav between pages
   const navigation = useNavigation();
 
+  // SETUP: DATA transfer stuff + NFP info
+  // const [handle, _] = React.useState(route.params);
+  // let DATA = DATA.PROFILES[handle];
   
   return(
 
       <SafeAreaView styles={styles.safeAreaContainer}>
-      <View style={styles.container}>
+      <ScrollView style={styles.scrollView}>
 
         <View style={styles.headerContainer}>
           <Text style={styles.titles}>NFP Profile</Text>
@@ -27,10 +30,46 @@ export default function Fundraiser() {
 
         
         <View style={styles.mainContent}>
-            <Text> Hello world.</Text>
+          <ImageBackground source={DATA.banner} style = {styles.bgimg} >
+          <Image source = {DATA.avatar} style = {styles.sideiconprof}></Image>
+          <Text style = {styles.whitetext}> {DATA.title} </Text>
+          <Text style = {styles.blacktext}> {DATA.name} </Text>
+          <Text style = {styles.blacktext}> {DATA.dates} </Text>
+          <Pressable style={styles.donatebutton} onPress={() => {
+            navigation.navigate('DonationEntry', handle)
+          }}
+          style={({ pressed }) => [
+            {
+              top: '13%',
+              left: '20%',
+              width: 250,
+              alignItems: 'center',
+              justifyContent: 'center',
+              paddingVertical: 12,
+              borderRadius: 100,
+              elevation: 3,
+              backgroundColor: pressed
+                ? 'gray'
+                : COLORS.GREEN
+            },
+            
+            styles.wrapperCustom
+          ]}>
+          <Text style={styles.buttontext}> DONATE </Text> 
+          </Pressable>
+          <Image style = {styles.infotext} source = {require('../../assets/Donation/FundraiserDescription.png')} > 
+        </Image>
+        <Image style = {styles.infotext} source = {require('../../assets/Donation/RewardBlurb.png')} > 
+        </Image>
+        <Image style = {styles.info} source = {require('../../assets/Donation/ImpactStats.png')} > 
+        </Image>
+        <Image style = {styles.info} source = {require('../../assets/Donation/VideoContent.png')} > 
+        </Image>
+
+        </ImageBackground>
             
         </View>
-      </View>
+      </ScrollView>
       </SafeAreaView>
   );
 }
@@ -39,9 +78,8 @@ const styles = StyleSheet.create({
     safeAreaContainer: {
       backgroundColor: 'white',
     },
-    container: {
+    scrollView: {
       backgroundColor: 'white',
-      height: '100%',
     },
     mainContent: { // container view
       // backgroundColor: 'firebrick',
@@ -59,6 +97,42 @@ const styles = StyleSheet.create({
       paddingTop: 25,
       textAlign: 'center',
       letterSpacing: 4,
+    },
+    bgimg: {
+      display: "flex",
+      width: '100%',
+      height: '35%',
+      position: 'relative',
+    },
+    sideiconprof: {
+      borderRadius: 100,
+      margin: 7,
+      top: '20%',
+      left: '6%',
+      height: 100,
+      width: 100,
+    }, 
+    whitetext: {
+      top: '6%',
+      left: '34%',
+      fontSize: 30,
+      color: 'white',
+      fontWeight: 'bold',
+    },
+    blacktext: {
+      top: '10%',
+      left: '34%',
+      fontSize: 18,
+      color: 'black',
+    },
+    infotext: {
+      top: '17%',
+      left: '3%',
+      marginBottom: 20,
+    },
+    info: {
+      top: '17%',
+      marginBottom: 20,
     },
     buttonSize: { // handle image style of small button
       resizeMode: 'stretch', 
@@ -78,6 +152,12 @@ const styles = StyleSheet.create({
       left: 80,
     },
     axnButtonText: {
+      color: COLORS.WHITE,
+      fontWeight: 'bold',
+      fontSize: 35,
+    },
+    // compare this to above
+    buttontext: {
       color: COLORS.WHITE,
       fontWeight: 'bold',
       fontSize: 35,
