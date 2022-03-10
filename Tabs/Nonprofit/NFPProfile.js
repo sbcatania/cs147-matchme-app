ff116038b2f0807cf1cab4e05d69d783d064a378
Lily Taylor <lilianajtaylor@stanford.edu>
2022-03-09 17:20:27 -0800

    Saving this working colored version

--- Tabs/Nonprofit/NFPProfile.js
+++ Tabs/Nonprofit/NFPProfile.js
@@ -9,6 +9,7 @@ import { COLORS, DATA } from '../../Constants';
 *   - Implement NFP profile page.
 *   - WHITE BACKGROUND - do the hacky safe area view thing to get a completely white bacgkround
 */
+
 //export default function NFPFundraiser({route}) {
 export default function NFPFundraiser() {
 
@@ -25,46 +26,46 @@ export default function NFPFundraiser() {
       <ScrollView style={styles.scrollView}>
 
         <View style={styles.headerContainer}>
+          <Image></Image>
           <Text style={styles.titles}>NFP Profile</Text>
         </View>
-
         
         <View style={styles.mainContent}>
           <ImageBackground source={DATA.banner} style = {styles.bgimg} >
-          <Image source = {DATA.avatar} style = {styles.sideiconprof}></Image>
-          <Text style = {styles.whitetext}> {DATA.title} </Text>
-          <Text style = {styles.blacktext}> {DATA.name} </Text>
-          <Text style = {styles.blacktext}> {DATA.dates} </Text>
-          <Pressable style={styles.donatebutton} onPress={() => {
-            navigation.navigate('DonationEntry', handle)
-          }}
-          style={({ pressed }) => [
-            {
-              top: '13%',
-              left: '20%',
-              width: 250,
-              alignItems: 'center',
-              justifyContent: 'center',
-              paddingVertical: 12,
-              borderRadius: 100,
-              elevation: 3,
-              backgroundColor: pressed
-                ? 'gray'
-                : COLORS.GREEN
-            },
-            
-            styles.wrapperCustom
-          ]}>
-          <Text style={styles.buttontext}> DONATE </Text> 
-          </Pressable>
-          <Image style = {styles.infotext} source = {require('../../assets/Donation/FundraiserDescription.png')} > 
-        </Image>
-        <Image style = {styles.infotext} source = {require('../../assets/Donation/RewardBlurb.png')} > 
-        </Image>
-        <Image style = {styles.info} source = {require('../../assets/Donation/ImpactStats.png')} > 
-        </Image>
-        <Image style = {styles.info} source = {require('../../assets/Donation/VideoContent.png')} > 
-        </Image>
+            <Image source = {DATA.avatar} style = {styles.sideiconprof}></Image>
+            <Text style = {styles.whitetext}> {DATA.title} </Text>
+            <Text style = {styles.blacktext}> {DATA.name} </Text>
+            <Text style = {styles.blacktext}> {DATA.dates} </Text>
+            <Pressable style={styles.donatebutton} onPress={() => {
+              // navigation.navigate('DonationEntry', handle)
+            }}
+            style={({ pressed }) => [
+              {
+                top: '13%',
+                left: '20%',
+                width: 250,
+                alignItems: 'center',
+                justifyContent: 'center',
+                paddingVertical: 12,
+                borderRadius: 100,
+                elevation: 3,
+                backgroundColor: pressed
+                  ? 'gray'
+                  : COLORS.GREEN
+              },
+              
+              styles.wrapperCustom
+            ]}>
+            <Text style={styles.buttontext}> DONATE </Text> 
+            </Pressable>
+            <Image style = {styles.infotext} source = {require('../../assets/Donation/FundraiserDescription.png')} > 
+          </Image>
+          <Image style = {styles.infotext} source = {require('../../assets/Donation/RewardBlurb.png')} > 
+          </Image>
+          <Image style = {styles.info} source = {require('../../assets/Donation/ImpactStats.png')} > 
+          </Image>
+          <Image style = {styles.info} source = {require('../../assets/Donation/VideoContent.png')} > 
+          </Image>
 
         </ImageBackground>
             
@@ -77,6 +78,7 @@ export default function NFPFundraiser() {
 const styles = StyleSheet.create({
     safeAreaContainer: {
       backgroundColor: 'white',
+      flex: 1,
     },
     scrollView: {
       backgroundColor: 'white',
@@ -99,40 +101,47 @@ const styles = StyleSheet.create({
       letterSpacing: 4,
     },
     bgimg: {
+      backgroundColor: 'red',
       display: "flex",
       width: '100%',
       height: '35%',
       position: 'relative',
     },
     sideiconprof: {
+      backgroundColor: 'pink',
       borderRadius: 100,
       margin: 7,
       top: '20%',
-      left: '6%',
+      // left: '6%',
       height: 100,
       width: 100,
     }, 
     whitetext: {
+      backgroundColor: 'blue',
       top: '6%',
-      left: '34%',
+      // left: '34%',
       fontSize: 30,
       color: 'white',
       fontWeight: 'bold',
     },
     blacktext: {
+      backgroundColor: 'purple',
       top: '10%',
-      left: '34%',
+      // left: '34%',
       fontSize: 18,
       color: 'black',
     },
     infotext: {
+      backgroundColor: 'brown',
       top: '17%',
-      left: '3%',
+      // left: '3%',
       marginBottom: 20,
     },
     info: {
+      backgroundColor: 'green',
       top: '17%',
       marginBottom: 20,
+      width: '99%',
     },
     buttonSize: { // handle image style of small button
       resizeMode: 'stretch', 
