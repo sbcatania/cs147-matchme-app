8b60d1bd014893f6d0775593427e35dc01ab9c34
Lily Taylor <lilianajtaylor@stanford.edu>
2022-03-10 21:37:19 -0800

    Working fundraiser on Donor side

--- Tabs/Donation/Fundraiser.js
+++ Tabs/Donation/Fundraiser.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, ImageBackground, Pressable, View, Image, TouchableOpacity, ScrollView, SafeAreaView, Button, TouchableWithoutFeedback } from "react-native";
+import { StyleSheet, Text, Pressable, View, Image, ScrollView, SafeAreaView } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 import { useRoute } from "@react-navigation/native";
 import DonationEntry from './DonationEntry';
@@ -19,103 +19,161 @@ const Fundraiser = () => {
     const navigation = useNavigation();
 
     return(
-      <SafeAreaView style={styles.container}>
-        <ScrollView style={styles.scrollView}>
+      <View style={styles.container}>
+      <SafeAreaView>
+      <ScrollView style={styles.scrollView}>
 
-        <ImageBackground source={data.banner} style = {styles.bgimg} >
-        <Image source = {data.avatar} style = {styles.sideiconprof}> 
-        </Image>
-        <Text style = {styles.whitetext}> {data.title} </Text>
-        <Text style = {styles.blacktext}> {data.name} </Text>
-        <Text style = {styles.blacktext}> {data.dates} </Text>
-        <Pressable style={styles.donatebutton} onPress={() => {
-          navigation.navigate('DonationEntry', handle)
-        }}
-         style={({ pressed }) => [
-          {
-            top: '13%',
-            left: '20%',
-            width: 250,
-            alignItems: 'center',
-            justifyContent: 'center',
-            paddingVertical: 12,
-            borderRadius: 100,
-            elevation: 3,
-            backgroundColor: pressed
-              ? 'gray'
-              : COLORS.GREEN
-          },
+        {/* Moose, different margins than everything else! */}
+        <View>
+          <Image source={require('../../assets/Donation/moose.png')} style={styles.backgroundimg}></Image>
+        </View>
+        
+        {/* All the other content, 20px margins */}
+        <View style={styles.mainContent}>
+            
+          {/* Header */}
+          <View style={styles.header}> 
+              <Image source={require('../../assets/Nonprofit/wwf.png')} style={{resize: 'center', height: 100, width: 100,}}></Image>
+              <Text style = {styles.whitetext}>Save the Animals</Text>
+              <Text style = {styles.blacktext}>World Wildlife Fund{'\n'}MARCH 2022-FEBRUARY 2023</Text>
+          </View>
+
+          {/* Body Content */}
+          <View style={styles.body}>
+            
+            <View style={styles.pageElem}>
+              <Text style={styles.heading1}>
+                About this fundraiser
+              </Text>
+              <Text>
+                The Save the Animals campaign aims to ensure all animals have a habitat suited for their needs. Funds will go to conservation efforts worldwide.
+              </Text>
+            </View>
+            
+            <Pressable onPress={() => { // navigation.navigate('DonationEntry', handle)
+              }}
+              style={({ pressed }) => [
+                {
+                  left: '15%', 
+                  width: '70%',
+                  marginVertical: 10,
+                  alignItems: 'center', // text
+                  justifyContent: 'center', // text
+                  paddingVertical: 10,
+                  borderRadius: 100,
+                  elevation: 3,
+                  backgroundColor: pressed
+                    ? 'gray'
+                    : COLORS.GREEN
+                }]}>
+                <Text style={styles.buttontext}> DONATE </Text> 
+            </Pressable>
+
+            <View style={{backgroundColor: 'pink', height: 120, borderRadius: 10, margin: 10,}}>
+                <Text style={{textAlign: 'center', padding: 10}}>
+                  Rewards go here!
+                </Text>
+            </View>
+
+            <View style={styles.pageElem}>
+              <Text style={styles.heading1}>
+                Impact
+              </Text>
+              <Image source={require('../../assets/Nonprofit/impact.png')} style={{height: 350, width: '100%', resizeMode: 'contain', }}></Image>
+              
+          </View>
+
+          <View style={styles.pageElemFullScreen}>
+            <Image source={require('../../assets/Nonprofit/grid.png')} style={{height: 550, width: '100%', resizeMode: 'contain', }}></Image>
           
-          styles.wrapperCustom
-        ]}>
-            <Text style={styles.buttontext}> DONATE </Text> 
-    </Pressable>
-    <Image style = {styles.infotext} source = {require('../../assets/Donation/FundraiserDescription.png')} > 
-        </Image>
-        <Image style = {styles.infotext} source = {require('../../assets/Donation/RewardBlurb.png')} > 
-        </Image>
-        <Image style = {styles.info} source = {require('../../assets/Donation/ImpactStats.png')} > 
-        </Image>
-        <Image style = {styles.info} source = {require('../../assets/Donation/VideoContent.png')} > 
-        </Image>
+            <View style={{backgroundColor: 'white', height: 140, }}>
+              {/* This is whitespace, so we can scroll to the bottom */}
+            </View>
+          </View>
 
-        </ImageBackground>
-        </ScrollView>
+          </View>
+            
+        </View>
+
+      </ScrollView>
       </SafeAreaView>
+      </View>
 
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: COLORS.WHITE,
-    },
-    scrollView: {
-      backgroundColor: 'white',
-    },
-    buttontext: {
-      color: COLORS.WHITE,
-      fontWeight: 'bold',
-      fontSize: 35,
-    },
-  
-    bgimg: {
-      display: "flex",
-      width: '100%',
-      height: '35%',
-      position: 'relative',
-    },
-    sideiconprof: {
-        borderRadius: 100,
-        margin: 7,
-        top: '20%',
-        left: '6%',
-        height: 100,
-        width: 100,
-    }, 
-    whitetext: {
-      top: '6%',
-      left: '34%',
-      fontSize: 30,
-      color: 'white',
-      fontWeight: 'bold',
-    },
-    blacktext: {
-      top: '10%',
-      left: '34%',
-      fontSize: 18,
-      color: 'black',
-    },
-    infotext: {
-      top: '17%',
-      left: '3%',
-      marginBottom: 20,
-    },
-    info: {
-      top: '17%',
-      marginBottom: 20,
-    }
+  backgroundimg: {
+    position:'absolute', 
+    left: 0, 
+    top: 0,
+    height: 200,
+  },
+  container: {
+    backgroundColor: 'white',
+    height: '100%',
+  },
+  scrollView: {
+    backgroundColor: 'white',
+    height: '100%',
+  },
+  mainContent: { // container view
+    top: 200,
+    // backgroundColor: 'firebrick',
+    marginHorizontal: 20,
+  },
+  header: {
+    // backgroundColor: 'green',
+    top: -50,
+  },
+  circle: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+    backgroundColor: 'lightgreen',
+    top: 0,
+  },
+  whitetext: {
+    // backgroundColor: 'blue',
+    position: 'absolute',
+    bottom: 50+5,
+    left: 100+5,
+    width: 250-5,
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  blacktext: {
+    position: 'absolute',
+    // backgroundColor: 'purple',
+    top: 50+5,
+    left: 100+5,
+    width: 250-5,
+    fontSize: 16,
+    color: 'black',
+  },
+  body: { // a container, a view
+    top: -50+5,
+    // backgroundColor: 'lightblue',
+  },
+  heading1: {
+    fontWeight:'bold', 
+    fontSize: 24,
+    marginBottom: 5,
+  },  
+  pageElem: {
+    marginVertical: 10, 
+    // backgroundColor:'beige',
+  },
+  pageElemFullScreen: {
+    marginVertical: 10, 
+    marginHorizontal: -22,
+  },  
+  buttontext: {
+    color: COLORS.WHITE,
+    fontWeight: 'bold',
+    fontSize: 35,
+  },
 });
 
 export default Fundraiser;
